1bd6b650a2ebf156789f51d35a1f2d3f05f95d4b
Joe Inglish <joseph.inglish@gmail.com>
2016-01-10 21:26:27 -0800

    Added fs require; starting on command line arg and file reading

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-parser = require('csv-parse');
+'use strict';
+
+var parser = require('csv-parse');
+var fs = require('fs');
+
+var debug = false;
+//CSV file name is passed as a command line argument
+var file = process.argv[2];
+fs.readFile(file, 'utf8', function (err, data) {
+  
+});
